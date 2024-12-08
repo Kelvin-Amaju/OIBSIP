6ada27087d740eb0a3c337f0a8a0f87e17acaeaf
Kelvin-Amaju <kelvin_anonudo@outlook.com>
2024-12-08 11:09:04 -0800

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,10 @@
+//Javascript for Mobile Menu Toggle
+const menuToggler = document.getElementById('menu-toggler');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggler.addEventListener('click', () => {
+    const isOpen = mobileMenu.classList.toggle('open');
+    menuToggler.setAttribute('aria-expanded', isOpen);
+})
+
+document.getElementById('year').textContent = new Date().getFullYear();
